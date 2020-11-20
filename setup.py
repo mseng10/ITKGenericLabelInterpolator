0567c807a473f808fa146e56e8ff94fc2f08fa32
Mathew Seng <mathewseng@gmail.com>
2020-11-20 08:43:12 -0600

    ENH: Update python package version for modules This will bump the package version in setup.py from 1.0.0 to 1.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-genericlabelinterpolator',
-    version='1.0.0',
+    version='1.1.0',
     author='Joël Schaerer',
     author_email='joelthelion@laposte.net',
     packages=['itk'],
